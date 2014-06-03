b8913a2256130cd50a41eecdc424d356f04a3404
Rick Silterra <es287@cornell.edu>
2014-06-03 16:26:52 -0400

    correct for latest link text

--- webd/test009_wbd_rspec.rb
+++ webd/test009_wbd_rspec.rb
@@ -14,28 +14,28 @@ describe "The website" do
     _after
   end
 
-  it "should link to 'Requests' which shows the text 'Borrowing and Delivery'" do
-    @link_text = ' Borrow, Renew, Return'
-    @page_text = 'Borrowing'
+  it "should link to 'Borrow, Request, Renew, Return' which shows the text 'Borrow, Request, Renew, Return'" do
+    @link_text = 'Borrow, Request, Renew, Return'
+    @page_text = 'Borrow, Request, Renew, Return'
     @driver.get(@base_url + "/")
-    element_present?(:link, @link_text).should == true
-    @driver.find_element(:link, @link_text).click
+    element_present?(:partial_link_text, @link_text).should == true
+    @driver.find_element(:partial_link_text, @link_text).click
     @body_text = @driver.find_element(:css, "BODY").text
     text_found?(@page_text, @body_text)
   end
 
   it "should link to 'Requests' which shows the text 'Borrowing and Delivery', and links to Faculty and Staff page." do
-    @link_text = ' Borrow, Renew, Return'
-    @page_text = 'Borrowing'
+    @link_text = 'Borrow, Request, Renew, Return'
+    @page_text = 'Borrow, Request, Renew, Return'
     @driver.get(@base_url + "/")
     element_present?(:partial_link_text, @link_text).should == true
-    @driver.find_element(:link, @link_text).click
+    @driver.find_element(:partial_link_text, @link_text).click
     @body_text = @driver.find_element(:css, "BODY").text
     text_found?(@page_text, @body_text)
-    @link_text = 'Faculty and Staff'
-    @page_text = 'Faculty and Staff Policies'
-    element_present?(:link, @link_text).should == true
-    @driver.find_element(:link, @link_text).click
+    @link_text = 'Faculty'
+    @page_text = 'Cornell Faculty'
+    element_present?(:partial_link_text, @link_text).should == true
+    @driver.find_element(:partial_link_text, @link_text).click
     @body_text = @driver.find_element(:css, "BODY").text
     text_found?(@page_text, @body_text)
   end
